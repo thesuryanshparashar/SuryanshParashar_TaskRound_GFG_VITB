85214eb80c8f77f0d94c3e16b792c0292baf6d02
Suryansh Parashar <127846351+thesuryanshparashar@users.noreply.github.com>
2023-08-09 20:50:56 +0530

    the first upload

---
+++ script.js
@@ -0,0 +1,8 @@
+const event1 = document.querySelector("#event1")
+event1.addEventListener("click", () =>
+    location.href="https://www.instagram.com/p/Cp9xQ7avuwl/?utm_source=ig_web_copy_link&igshid=MzRlODBiNWFlZA=="
+)
+const event2 = document.querySelector("#event2")
+event2.addEventListener("click", () =>
+    location.href="https://www.instagram.com/p/CoEuExzPpHT/?utm_source=ig_web_copy_link"
+)
